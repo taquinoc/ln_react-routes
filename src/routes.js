afb00181d1eb846213c9414e4c4336fa8619d8a2
Thiago <thiagoaquino@wizsolucoes.com.br>
2019-08-30 14:29:29 -0300

    First commit

---
+++ src/routes.js
@@ -0,0 +1,9 @@
+/**Esse arquivo serve para definir rotas */
+
+import React from 'react'; /**O react foi importado porque será trabalhado com JSX */
+import { BrowserRoute, Route, Switch, Redirect } from 'react-router-dom';
+
+/**Stateless Functional Component é um arrow function. Ele é muito utilizado quando não há complexidade, ele só renderiza. */
+const routes = () => (
+
+);
