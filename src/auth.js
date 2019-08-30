42216af784f97f71cf8de832f60d9938b8963036
Thiago <thiagoaquino@wizsolucoes.com.br>
2019-08-30 16:28:19 -0300

    Second commit

---
+++ src/auth.js
@@ -0,0 +1,6 @@
+/**Como funciona uma autenticação no Frontend? Geralmente o usuário se autentica e aplicação leva dados de e-mail para um servidor e esse servidor retorna um Token JWT
+ * este token é guardado no storage da aplicação e geralmente na Web é salvo no local storage e qualquer requisição subsequente ele envia esse token junto para o nosso backend
+ */
+
+    /**Aqui haveria toda a lógica */
+export const isAuthenticated = () => true;
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,27 @@
 /**Esse arquivo serve para definir rotas */
 
 import React from 'react'; /**O react foi importado porque será trabalhado com JSX */
-import { BrowserRoute, Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { switchCase } from '@babel/types';
+import { isAuthenticated } from './auth';
+
+const PrivateRoute = ({component: Component, ...rest}) => (
+    <Route {...rest} render={props => (
+        isAuthenticated() ? (
+            <Component {...props} />
+        ) : (
+            <Redirect to = ({ pathname: '/', state: {from: props.location}})/>
+        )
+        )} />
+);
 
 /**Stateless Functional Component é um arrow function. Ele é muito utilizado quando não há complexidade, ele só renderiza. */
-const routes = () => (
-
+const Routes = () => (
+    <BrowserRouter>
+        <Switch> {/**O switch faz com que ele abra somente a primeira rota que ele encontrar */}
+            <Route exact path="/" component={() => <h1>Hello World</h1>} />
+        </Switch>
+    </BrowserRouter>
 );
+
+export default Routes;
